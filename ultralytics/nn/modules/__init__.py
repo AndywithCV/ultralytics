e01480399c2c571774820c98788f25af6e698418
AndywithCV <94813960+AndywithCV@users.noreply.github.com>
2023-10-30 19:54:49 +0800

    Update __init__.py

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -1,26 +1,9 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-"""
-Ultralytics modules.
 
-Example:
-    Visualize a module with Netron.
-    ```python
-    from ultralytics.nn.modules import *
-    import torch
-    import os
-
-    x = torch.ones(1, 128, 40, 40)
-    m = Conv(128, 128)
-    f = f'{m._get_name()}.onnx'
-    torch.onnx.export(m, x, f)
-    os.system(f'onnxsim {f} {f} && open {f}')
-    ```
-"""
 
 from .block import (C1, C2, C3, C3TR, DFL, SPP, SPPF, Bottleneck, BottleneckCSP, C2f, C3Ghost, C3x, GhostBottleneck,
-                    HGBlock, HGStem, Proto, RepC3)
+                    HGBlock, HGStem, Proto, RepC3, C2fGhost, CARAFE, SPPFCSPC, SimSPPF, C2f_DCN, Bottleneck_DCN, Focus, SPPCSPC_group, SPPCSPC, GhostSPPCSPC)
 from .conv import (CBAM, ChannelAttention, Concat, Conv, Conv2, ConvTranspose, DWConv, DWConvTranspose2d, Focus,
-                   GhostConv, LightConv, RepConv, SpatialAttention)
+                   GhostConv, LightConv, RepConv, SpatialAttention, NAMAttention, ShuffleAttention, ECAAttention, SEAttention, GAMAttention)
 from .head import Classify, Detect, Pose, RTDETRDecoder, Segment
 from .transformer import (AIFI, MLP, DeformableTransformerDecoder, DeformableTransformerDecoderLayer, LayerNorm2d,
                           MLPBlock, MSDeformAttn, TransformerBlock, TransformerEncoderLayer, TransformerLayer)
@@ -30,4 +13,4 @@ __all__ = ('Conv', 'Conv2', 'LightConv', 'RepConv', 'DWConv', 'DWConvTranspose2d
            'TransformerBlock', 'MLPBlock', 'LayerNorm2d', 'DFL', 'HGBlock', 'HGStem', 'SPP', 'SPPF', 'C1', 'C2', 'C3',
            'C2f', 'C3x', 'C3TR', 'C3Ghost', 'GhostBottleneck', 'Bottleneck', 'BottleneckCSP', 'Proto', 'Detect',
            'Segment', 'Pose', 'Classify', 'TransformerEncoderLayer', 'RepC3', 'RTDETRDecoder', 'AIFI',
-           'DeformableTransformerDecoder', 'DeformableTransformerDecoderLayer', 'MSDeformAttn', 'MLP')
+           'DeformableTransformerDecoder', 'DeformableTransformerDecoderLayer', 'MSDeformAttn', 'MLP', 'C2fGhost', 'CARAFE', 'SPPFCSPC', 'SimSPPF', 'NAMAttention', 'C2f_DCN', 'Bottleneck_DCN', 'Focus', 'ShuffleAttention', 'ECAAttention', 'SEAttention', 'GAMAttention', 'SPPCSPC_group', 'SPPCSPC', 'GhostSPPCSPC')
